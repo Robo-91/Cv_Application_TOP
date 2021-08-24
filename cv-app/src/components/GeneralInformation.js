e51d2c8a3ca6a480801c9798e75193a89c6b3858
Robert <r91palmer@gmail.com>
2021-08-24 15:44:09 -0500

    added state and handler functions for inputs

--- cv-app/src/components/GeneralInformation.js
+++ cv-app/src/components/GeneralInformation.js
@@ -1,14 +1,37 @@
+import React,{ useState } from "react";
+
 const GeneralInformation = () => {
+    const [enteredFirstName, setEnteredFirstName] = useState('');
+    const [enteredLastName, setEnteredLastName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPhoneNum, setEnteredPhoneNum] = useState('');
+
+    const setFirstNameHandler = (event) => {
+        setEnteredFirstName(event.target.value);
+    };
+
+    const setLastNameHandler = (event) => {
+        setEnteredLastName(event.target.value);
+    };
+
+    const setEmailHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    };
+
+    const setPhoneNumberHandler = (event) => {
+        setEnteredPhoneNum(event.target.value);
+    };
+
     return (
         <div>
             <label htmlFor="fname">First Name:</label>
-            <input id="fname" type="text" />
+            <input id="fname" type="text" value={enteredFirstName} onChange={setFirstNameHandler} />
             <label htmlFor="lname">Last Name:</label>
-            <input id="lname" type="text" />
+            <input id="lname" type="text" value={enteredLastName} onChange={setLastNameHandler} />
             <label htmlFor="email">Email:</label>
-            <input id="email" type="email" />
+            <input id="email" type="email" value={enteredEmail} onChange={setEmailHandler} />
             <label htmlFor="phonenumber">Telephone Number:</label>
-            <input id="phonenumber" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
+            <input id="phonenumber" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={enteredPhoneNum} onChange={setPhoneNumberHandler} />
         </div>
     ); 
 };
